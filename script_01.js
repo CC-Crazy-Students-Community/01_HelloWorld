e59db7a5d9c925896209bc610c9a6c9aa2727254
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-11 00:43:46 +0100

    Output generieren

--- script_01.js
+++ script_01.js
@@ -1,6 +1,6 @@
-/***********************************************************************/
-/*********  Hello World  |  console log  |  output generiern  **********/
-/***********************************************************************/
+/************************************************************************/
+/*********  Hello World!  |  console log  |  output generiern  **********/
+/************************************************************************/
 /*
     console ist eine vordefinierte Funktion, mit welcher man weitere Unterfunktionen wie zb log (für logfile, logtext)
     aufrufen kann und damit eben auch eine Asugabe machen kann
