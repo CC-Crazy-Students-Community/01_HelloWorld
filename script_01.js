e1797d44d15bd5b5e2af46de3616ea32433bb279
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-11 00:41:43 +0100

    output generiern

--- script_01.js
+++ script_01.js
@@ -1,11 +1,8 @@
-/*********   Hello World!   **********/
-
+/***********************************************************************/
+/*********  Hello World  |  console log  |  output generiern  **********/
+/***********************************************************************/
 /*
     console ist eine vordefinierte Funktion, mit welcher man weitere Unterfunktionen wie zb log (für logfile, logtext)
     aufrufen kann und damit eben auch eine Asugabe machen kann
 */
-
-
 console.log("Hallo World!");
-
-
